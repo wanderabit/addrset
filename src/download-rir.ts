54f18b0f4c7314ccaa970f9ec7e17b684741e8a3
wander <247986740+wanderabit@users.noreply.github.com>
2025-12-26 13:03:58 +0800

    update: download-rir.ts retry

--- src/download-rir.ts
+++ src/download-rir.ts
@@ -5,11 +5,26 @@ import ky from "ky";
 
 await makeAndCleanDir("./dist/rir");
 
-await Promise.all(
-    Object.entries(RIR).map(async ([name, url]) => {
-        const resp = await ky.get(url, { retry: 5, timeout: 1000 * 60 });
-        const data = await resp.text();
-        await fs.promises.writeFile(`./dist/rir/delegated-${name}-extended-latest`, data);
-        console.log(`Download delegated-${name}-extended-latest finish`);
-    }),
-);
+async function download(url: string, path: string) {
+    let retry = 0;
+
+    while (true) {
+        if (retry >= 3) {
+            throw new Error(`Download ${url} failed after ${retry} attempts`);
+        }
+
+        try {
+            retry += 1;
+            console.log(`Download ${url} to ${path}`);
+            const resp = await ky.get(url, { retry: 5, timeout: 1000 * 60 });
+            const data = await resp.text();
+            await fs.promises.writeFile(path, data);
+            console.log(`Download ${path} finish`);
+            return;
+        } catch (err) {
+            console.error(`Download ${url} occurred error on attempt ${retry}:`, err);
+        }
+    }
+}
+
+await Promise.all(Object.entries(RIR).map(([name, url]) => download(url, `./dist/rir/delegated-${name}-extended-latest`)));
