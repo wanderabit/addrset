995dc7cf14282a55a4223295f649810be8f445dd
wander <247986740+wanderabit@users.noreply.github.com>
2026-01-11 10:03:17 +0800

    update: download-rir.ts retry delay

--- src/download-rir.ts
+++ src/download-rir.ts
@@ -13,9 +13,18 @@ async function download(url: string, path: string) {
             throw new Error(`Download ${url} failed after ${retry} attempts`);
         }
 
+        console.log(`Download ${url} to ${path}`);
+        
+        retry += 1;
+        console.log(`Download ${url} attempt ${retry}`);
+
+        if (retry > 1) {
+            const delay = 2000 * (retry - 1);
+            console.log(`Wait for ${delay} ms before retrying`);
+            await new Promise((resolve) => setTimeout(resolve, delay));
+        }
+
         try {
-            retry += 1;
-            console.log(`Download ${url} to ${path}`);
             const resp = await ky.get(url, { retry: 5, timeout: 1000 * 60 });
             const data = await resp.text();
             await fs.promises.writeFile(path, data);
